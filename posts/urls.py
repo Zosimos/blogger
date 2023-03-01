26e29c97f1cca52b4ca70a2575f3e2265f7392b6
Jim Barnett <hlbradow@yahoo.com>
2023-02-28 20:04:16 -0500

    day 3

--- posts/urls.py
+++ posts/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
     path("", PostListView.as_view(), name="list"),
-    path("int:pk>/edit/", PostUpdateView.as_view(), name="edit"),
+    path("<int:pk>/edit/", PostUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete/", PostDeleteView.as_view(), name="delete"),
     path("<int:pk>/", PostDetailView.as_view(), name="detail"),
     path("new/", PostCreateView.as_view(), name="new"),
